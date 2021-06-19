2348180698f12ee8da24793255e34ac107911c02
andrasimon <andrasimon8@gmail.com>
2021-06-20 00:49:41 +0300

    Some more work on HW2.

--- HW2.py
+++ HW2.py
@@ -11,7 +11,36 @@ def real_sum(*args, **kwargs):
     return suma
 
 
+# suma tuturor numerelor de la [0, n] recursiv
+def sum_n(n):
+    if n == 0:
+        return 0
+    return n + sum_n(n - 1)
+
+
+# suma numerelor pare de la [0, n]
+def sum_n_even(n):
+    n = n - n % 2
+    if n == 0:
+        return 0
+    return n + sum_n_even(n - 2)
+
+
+# suma numerelor impare de la [0, n]
+def sum_n_odd(n):
+    n = n - int(not (n % 2))
+    if n == 1:
+        return 1
+    return n + sum_n_odd(n - 2)
+
+
 if __name__ == '__main__':
-    print(real_sum(1, 5, -3, 'random', [5, 6, 7, 8], param=7, param2='hi'))
-    print(real_sum(4, 8, 9, 'real'))
-    print(real_sum())
+    print('real_sum 1st run --> {}'.format(real_sum(1, 5, -3, 'random', [5, 6, 7, 8], param=7, param2='hi')))
+    print('real_sum 2nd run --> {}'.format(real_sum(4, 8, 9, 'real')))
+    print('real_sum 3rd run --> {}'.format(real_sum()))
+
+    print('sum_n(3) --> {}'.format(sum_n(3)))
+
+    print('sum_n_even(5) --> {}'.format(sum_n_even(5)))
+
+    print('sum_n_odd(6) --> {}'.format(sum_n_odd(6)))
